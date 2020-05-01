afa4a3c020056e906ebaba776f8a97a3c4b8d7b0
Tony Rim <duc_thinh_03@yahoo.com>
2020-05-01 21:30:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
+  def self.print_all 
+
+    @@all.each do |name|
+      puts "#{name}"
+    end 
+  end
+  
+  
+  def save 
+    @@all << self 
+  end 
+  
+  
+  def self.clear_all 
+    @@all.clear 
+  end 
+
+end 
